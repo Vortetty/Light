c6316a51f16e14249c8b576a7ed8eda9b17ff713
Vortetty <33466216+Vortetty@users.noreply.github.com>
2019-12-13 11:29:18 -0800

    Update FX.js

--- FX.js
+++ FX.js
@@ -1,8 +1,10 @@
+//this part works
 const lightProduce = newEffect(20, e => {
     Draw.color(Color.white, Color.newColor(0x00ffff00), e.fin()); //color goes from white to a transparent blue
     Lines.swirl(0, 0, e.fin() * 100, 10, e.fin() * 360);
 });
 
+//this is iffy, might work might not, just producing effect on block update
 const extractor = extendContent(Block, "lightextractor", {
 
     update(Tile tile) {
