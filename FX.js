1443d14cdab7f0d7b0ca59c5de7595210f3a0b91
Vortetty <33466216+Vortetty@users.noreply.github.com>
2019-12-13 11:28:37 -0800

    Update FX.js

--- FX.js
+++ FX.js
@@ -1,6 +1,6 @@
 const lightProduce = newEffect(20, e => {
     Draw.color(Color.white, Color.newColor(0x00ffff00), e.fin()); //color goes from white to a transparent blue
-    Lines.swirl(0, 0, e.fin() * 100, e.fin() * 360);
+    Lines.swirl(0, 0, e.fin() * 100, 10, e.fin() * 360);
 });
 
 const extractor = extendContent(Block, "lightextractor", {
