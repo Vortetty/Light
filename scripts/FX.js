2af9f47785ecdb01e3dd9c297d71aa5e314c628f
Vortetty <33466216+Vortetty@users.noreply.github.com>
2020-01-14 11:37:50 -0800

    Update FX.js

--- scripts/FX.js
+++ scripts/FX.js
@@ -4,6 +4,11 @@ const lightProduce = newEffect(20, e => {
     Lines.swirl(0, 0, e.fin() * 100, 10, e.fin() * 360);
 });
 
+const siloLaunchEffect = newEffect(20, e => {
+    Draw.color(Color.white, Color.newColor(0x00ffff00), e.fin()); //color goes from white to a transparent blue
+    Lines.swirl(0, 0, e.fin() * 100, 10, e.fin() * 360);
+});
+
 //this is iffy, might work might not, just producing effect on block update and rebuilding the update thing so it works like a normal producer.
 const extractor = extendContent(Block, "lightextractor", {
 
