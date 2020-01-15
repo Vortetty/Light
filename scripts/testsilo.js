7736f88559e9f0bd5c7ca409637665b29c9c84fc
Vortetty <33466216+Vortetty@users.noreply.github.com>
2020-01-14 20:24:48 -0800

    Update testsilo.js

--- scripts/testsilo.js
+++ scripts/testsilo.js
@@ -1,5 +1,5 @@
 const LaunchEffect = newEffect(20, e => {
-    Draw.color(0x00ffffff); //color goes from white to a transparent blue
+    Draw.color(0x00ffffff,0x00000000,e.fin()); //color goes from ??? to ???
     for(var i = 0; i < 361; i++){
         Lines.lineAngle(e.x+25, e.y+25, i, e.fin() * 100); //draw a bunch of lines
         Lines.lineAngle(e.x-25, e.y+25, i, e.fin() * 100); //draw a bunch of lines
